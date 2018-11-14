5e5d213c3a767bda23e9d8df82b277b1ed2fee49
coded-textiles <44756679+coded-textiles@users.noreply.github.com>
2018-11-14 12:38:04 -0500

    Add files via upload

---
+++ sketches/four_corner_pathway_fragments_bezier/sketch.js
@@ -0,0 +1,247 @@
+/* Moving in a clockwise direction, connect each corner
+ * of a square to a random point on an opposing edge
+ * using a Bezier curve.
+ *
+ * Use three sliders to change the number of columns,
+ * rotation angle, and draw speed (i.e. frame rate) for
+ * the sketch.
+ *  
+ * Anna Garbier
+ * 2018-10-30
+ */
+
+
+// Basic canvas and program configuration
+const canvasWidth = 725;
+const canvasHeight = 725;
+
+let numCols = 1;
+let rotateAngle = 45;
+let framesPerSecond = 50;
+
+let squareStrokeColor = 220;
+const connectorStrokeColor = 0;
+const backgroundColor = 255;
+
+// Size of square is determined by canvas size
+// and the number of columns
+let squareDiagonal = canvasWidth / numCols;
+let squareWidth;
+let squareWidthHalved;
+let squareDiagonalHalved;
+let bezierRandomValue;
+
+// Variables used to navigate the space of
+// the canvas throughout draw
+let rowStartX;
+let rowStartY;
+let cornerCounter = 0;
+let colCounter = 0;
+let rowCounter = 0;
+
+function setup() {
+	var canvas = createCanvas(canvasWidth, canvasHeight);
+	canvas.parent('sketch-container');
+	background(backgroundColor);
+	noFill();
+	angleMode(DEGREES);
+	rectMode(CENTER);
+	ellipseMode(CENTER);
+	frameRate(framesPerSecond);
+
+	squareWidth = squareDiagonal / sqrt(2);
+	squareWidthHalved = squareWidth / 2;
+	squareDiagonalHalved = squareDiagonal / 2;
+	numRows = Math.floor(canvasHeight / squareDiagonal);
+
+	// Make refresh button and sliders
+	var button = createButton("Refresh sketch");
+	button.parent('start-button-container');
+	button.mousePressed(refreshSketch);
+
+	// Control sliders
+	// Number of columns
+	var numColsText = createP("Number of columns (1 to 20)");
+	numColsSlider = createSlider(1, 20, 1);
+	numColsText.parent('controls-container');
+	numColsSlider.parent('controls-container');
+	numColsSlider.class('slider');
+
+	// Tilt
+	var tiltAngleText = createP("Rotation angle (0 to 45 deg.)");
+	tiltAngleSlider = createSlider(0, 45, 45);
+	tiltAngleText.parent('controls-container');
+	tiltAngleSlider.parent('controls-container');
+	tiltAngleSlider.class('slider');
+
+	// Bezier curves
+	// var bezierRandomText = createP("Bezier range (0 to 80)");
+	// bezierRandomSlider = createSlider(0, 40, 0);
+	// bezierRandomSlider.parent('controls-container');
+	// bezierRandomSlider.parent('controls-container');
+	// bezierRandomSlider.class('slider');
+
+	// Frame rate
+	var frameRateText = createP("Frames per second (1 to 100)");
+	frameRateSlider = createSlider(1, 100, 10);
+	frameRateText.parent('controls-container');
+	frameRateSlider.parent('controls-container');
+	frameRateSlider.class('slider');
+
+	// Border color
+	var borderColorText = createP("Border color (black to white)");
+	squareStrokeColorSlider = createSlider(0, 255, 220);
+	borderColorText.parent('controls-container');
+	squareStrokeColorSlider.parent('controls-container');
+	squareStrokeColorSlider.class('slider');
+}
+
+function draw() {
+	if (rowCounter < numRows) {
+		drawRow();
+	}
+}
+
+// drawLine() is an UNUSED FUNCTION, left here for posterity.
+// It can be used within the drawBlock() function as an alternate
+// to the current drawBezier() to create straight connecting lines
+// rather than curved ones.
+function drawLine(startX, startY) {
+	// Pick a random number, 1 or 0. This is used later
+	// to randomly pick one of two sides to "attach to".
+	let sideSelector = Math.round(random(0, 1));
+
+	// Pick a random point on one of the two opposed
+	// edges from (startX, startY). .95 means we never
+	// connect to a point that is super close to a corner
+	if (sideSelector == 1) {
+		endX = -1 * startX;
+		endY = .95 * random(-squareWidthHalved, squareWidthHalved);
+	} else {
+		endX = .95 * random(-squareWidthHalved, squareWidthHalved);
+		endY = -1 * startY;
+	}
+
+	// Connect the corner at (startX, startY) to a random
+	// point on one of two opposite edges
+	line(startX, startY, endX, endY);
+}
+
+function drawBezier(startX, startY) {
+	// Pick a random number, 1 or 0. This is used later
+	// to randomly pick one of two sides to "attach to".
+	let sideSelector = Math.round(random(0, 1));
+
+	// Pick a random point on one of the two opposed
+	// edges from (startX, startY). .8 means we never
+	// connect to a point that is too close to another point
+	if (sideSelector == 1) {
+		endX = -1 * startX;
+		endY = .8 * random(-squareWidthHalved, squareWidthHalved);
+	} else {
+		endX = .8 * random(-squareWidthHalved, squareWidthHalved);
+		endY = -1 * startY;
+	}
+
+	// Connect the corner at (startX, startY) to a random
+	// point on one of two opposite edges using a Bezier curve
+	// with middle points of (0,0) and (0,0).
+	bezierRandomValue = 0;
+
+	// Optionally configure the curve further using slider input
+	// and/or randomization.
+	// if (bezierRandomSlider.value() == 0) {
+	// 	bezierRandomValue = 0;
+	// } else {
+	// 	bezierRandomValue = random(
+	// 		-1 * bezierRandomSlider.value(),
+	// 		bezierRandomSlider.value());
+	// }
+
+	bezier(startX,
+		startY,
+		bezierRandomValue,
+		bezierRandomValue,
+		bezierRandomValue,
+		bezierRandomValue,
+		endX,
+		endY);
+}
+
+// Draw a box, with four internal "connectors", each of which
+// connects a corner to a random point on an opposing edge
+function drawBlock() {
+	console.log("Row " + rowCounter +
+				", Column " + colCounter +
+				", Corner "+ cornerCounter);
+	if (cornerCounter == 0) {
+		stroke(squareStrokeColor);
+		rect(0, 0, squareWidth, squareWidth);
+		stroke(connectorStrokeColor);
+		cornerCounter++;
+	}
+	else if (cornerCounter == 1) {
+		// Use drawLine() instead for straight lines
+		drawBezier(-1 * squareWidthHalved, -1 * squareWidthHalved);
+		cornerCounter++;
+	} else if (cornerCounter == 2) {
+		drawBezier(squareWidthHalved, -1 * squareWidthHalved);
+		cornerCounter++;
+	} else if (cornerCounter == 3) {
+		drawBezier(squareWidthHalved, squareWidthHalved);
+		cornerCounter++;
+	} else if (cornerCounter == 4) {
+		drawBezier(-1 * squareWidthHalved, squareWidthHalved);
+		// Move on to the next column
+		colCounter++;
+		// Add a spacer in console output for readability
+		console.log(""); 
+		// Reset the corner counter to queue up a new block
+		cornerCounter = 0;
+	}
+}
+
+// Draw a row of blocks
+function drawRow() {
+	if (colCounter <= numCols) {
+		rowStartX = squareDiagonalHalved;
+		rowStartY = squareDiagonalHalved +
+					(squareDiagonal * rowCounter);
+
+		translateStepX = sqrt(sq(squareWidth) + sq(squareWidth));
+		translateStepY = translateStepX / 2;
+		x = rowStartX + (translateStepX * colCounter);
+		y = rowStartY;
+		translate(x, y);
+		rotate(rotateAngle);
+		drawBlock();
+	} else {
+		// Move to the next row, and reset the column counter
+		rowCounter++;
+		colCounter = 0;
+	}
+}
+
+function refreshSketch() {
+	background(backgroundColor);
+	frameRate(frameRateSlider.value());
+	numCols = numColsSlider.value();
+	rotateAngle = tiltAngleSlider.value();
+	squareStrokeColor = squareStrokeColorSlider.value();
+
+	squareDiagonal = canvasWidth / numCols;
+	squareWidth = squareDiagonal / sqrt(2);
+	squareWidthHalved = squareWidth / 2;
+	squareDiagonalHalved = squareDiagonal / 2;
+	numRows = Math.floor(canvasHeight / squareDiagonal);
+	cornerCounter = 0;
+	colCounter = 0;
+	rowCounter = 0;
+
+	// console.log("bezierRandomSlider: " + bezierRandomSlider.value());
+	console.log("frameRateSlider: " + frameRateSlider.value());
+	console.log("tiltAngleSlider: " + tiltAngleSlider.value());
+	console.log("numColsSlider: " + numColsSlider.value());
+	console.log("squareStrokeColorSlider: " + squareStrokeColorSlider.value());
+	console.log("---");
+}
---
+++ sketches/four_corner_unit_growth/sketch.js
@@ -0,0 +1,321 @@
+/* Generate a square with internal Bezier curves.
+ * Then systemmatically reflect that square to create
+ * larger, symmetrical squares of increasing complexity.
+ *
+ * TODO:
+ * 1) This is clumsily written; make it scalable.
+ * 2) Show starter unit on side
+ *
+ * Anna Garbier
+ */
+
+const canvasWidth = 725;
+const canvasHeight = 725;
+
+let borderStrokeColor = 220;
+const backgroundColor = 255;
+
+let squareDiagonal = 175;
+let squareWidth;
+let squareWidthHalved;
+let bezierRandomValue;
+
+let rotateAngle = 45;
+
+let cornerCounter = 0;
+let cycleCounter = 0;
+// let masterCycleCounter = 0;
+
+let bezierAnchor;
+
+let framesPerSecond = 10;
+// const frameRateIncrement = .25;
+
+// Variables for storing coordinates
+var endPointA = [];
+var endPointB = [];
+var endPointC = [];
+var endPointD = [];
+var randomFloat = [];
+
+// Variables for storing bezier or line decisions
+var randomZeroOrOne = [];
+
+function setup() {
+	console.log("Start setup...")
+	var canvas = createCanvas(canvasWidth, canvasHeight);
+	canvas.parent('sketch-container');
+	background(backgroundColor);
+	noFill();
+	frameRate(framesPerSecond);
+	angleMode(DEGREES);
+	squareWidth = squareDiagonal / sqrt(2);
+	squareWidthHalved = squareWidth / 2;
+
+	// Generate some random floats for later
+	for (i = 0; i < 5; i++) {
+		randomFloat[i] = random(0, .9 * squareWidth);
+		console.log(randomFloat[i]);
+	}
+
+	// Generate some random binary decisions to be used
+	// for deciding between a curve or line later
+	for (i = 0; i < 5; i++) {
+		randomZeroOrOne[i] = Math.round(random(0, 1));
+		console.log(randomZeroOrOne[i]);
+	}
+
+	// Create control sliders
+	// Make refresh button and sliders
+	var button = createButton("Refresh sketch");
+	button.parent('start-button-container');
+	button.mousePressed(refreshSketch);
+
+	// Tilt
+	// var rotateAngleText = createP("Rotation angle (0 to 45 deg.)");
+	// rotateAngleSlider = createSlider(0, 45, 45);
+	// rotateAngleText.parent('controls-container');
+	// rotateAngleSlider.parent('controls-container');
+	// rotateAngleSlider.class('slider');
+
+	// Frame rate
+	var frameRateText = createP("Frames per second (1 to 100)");
+	frameRateSlider = createSlider(1, 100, 10);
+	frameRateText.parent('controls-container');
+	frameRateSlider.parent('controls-container');
+	frameRateSlider.class('slider');
+
+	// Border color
+	var borderColorText = createP("Border color (black to white)");
+	squareStrokeColorSlider = createSlider(0, 255, 220);
+	borderColorText.parent('controls-container');
+	squareStrokeColorSlider.parent('controls-container');
+	squareStrokeColorSlider.class('slider');
+
+	console.log("End setup...")
+}
+
+function draw() {
+	rotate(rotateAngle);
+	translate(squareDiagonal * 1.5, - squareDiagonal * 1.5);
+	drawSixteenBlocks();
+	console.log("Cycle: " + cycleCounter);
+	console.log("Corner: " + cornerCounter);
+	// console.log("Frames per Second: " + framesPerSecond);
+}
+
+// Custom functions
+function drawBorder(startX, startY) {
+	stroke(squareStrokeColorSlider.value());
+	rect(startX, startY, squareWidth, squareWidth);
+}
+
+function drawLine(startX, startY, endX, endY) {
+	stroke(0);
+	// Connect the corner at (startX, startY) to a random
+	// point on one of two opposite edges (endX, endY) using a
+	// Bezier curve.
+	line(startX, startY, endX, endY);
+}
+
+function drawBezier(startX, startY, endX, endY, bezierAnchorX, bezierAnchorY) {
+	stroke(0);
+	// Connect the corner at (startX, startY) to a random
+	// point on one of two opposite edges (endX, endY) using a
+	// Bezier curve.
+	bezier(startX,
+		startY,
+		bezierAnchorX,
+		bezierAnchorY,
+		bezierAnchorX,
+		bezierAnchorY,
+		endX,
+		endY);
+}
+
+// Randomly decide if we'll draw a line or a Bezier curve.
+function drawConnector(startX, startY, endX, endY, bezierAnchorX, bezierAnchorY, randomZeroOrOne) {
+	if (randomZeroOrOne == 0) {
+		drawLine(startX, startY, endX, endY);
+	} else {
+		drawBezier(startX, startY, endX, endY, bezierAnchorX, bezierAnchorY);
+	}
+}
+
+// function increaseFrameRate() {
+// 	framesPerSecond = framesPerSecond + frameRateIncrement;
+// }
+
+function drawBlock0() {
+ 	if (cornerCounter == 0) {
+		drawBorder(0, 0);
+		bezierX = squareWidth / 2;
+		bezierY = squareWidth / 2;
+		cornerCounter++;
+	}
+	else if (cornerCounter == 1) {
+		startX = 0;
+		startY = 0;
+		endPointA[0] = squareWidth;
+		endPointA[1] = randomFloat[0];
+		drawConnector(startX, startY, endPointA[0], endPointA[1], bezierX, bezierY, randomZeroOrOne[0]);
+		cornerCounter++;
+	} else if (cornerCounter == 2) {
+		startX = squareWidth;
+		startY = 0;
+		endPointB[0] = randomFloat[1];
+		endPointB[1] = squareWidth;
+		drawConnector(startX, startY, endPointB[0], endPointB[1], bezierX, bezierY, randomZeroOrOne[1]);
+		cornerCounter++;
+	} else if (cornerCounter == 3) {
+		startX = squareWidth;
+		startY = squareWidth;
+		endPointC[0] = 0;
+		endPointC[1] = randomFloat[2];
+		drawConnector(startX, startY, endPointC[0], endPointC[1], bezierX, bezierY, randomZeroOrOne[2]);
+		cornerCounter++;
+	} else if (cornerCounter == 4) {
+		startX = 0;
+		startY = squareWidth;
+		endPointD[0] = randomFloat[3];
+		endPointD[1] = 0;
+		drawConnector(startX, startY, endPointD[0], endPointD[1], bezierX, bezierY, randomZeroOrOne[3]);
+		console.log("drawBlock complete");
+		cornerCounter++;
+	}
+ }
+
+function drawBlock1() {
+ 	if (cornerCounter == 5) {
+		drawBorder(squareWidth, 0);
+		bezierX = (squareWidth / 2) * 3;
+		bezierY = squareWidth / 2;
+		cornerCounter++;
+	}
+	else if (cornerCounter == 6) {
+		startX = squareWidth;
+		startY = 0;
+		endPointB[0] = (2 * squareWidth) - endPointB[0];
+		drawConnector(startX, startY, endPointB[0], endPointB[1], bezierX, bezierY, randomZeroOrOne[1]);
+		cornerCounter++;
+	} else if (cornerCounter == 7) {
+		startX = 2 * squareWidth;
+		startY = 0;
+		drawConnector(startX, startY, endPointA[0], endPointA[1], bezierX, bezierY, randomZeroOrOne[0]);
+		cornerCounter++;
+	} else if (cornerCounter == 8) {
+		startX = 2 * squareWidth;
+		startY = squareWidth;
+		endPointD[0] = endPointD[0] + ((squareWidth - endPointD[0]) * 2);
+		drawConnector(startX, startY, endPointD[0], endPointD[1], bezierX, bezierY, randomZeroOrOne[3]);
+		cornerCounter++;
+	} else if (cornerCounter == 9) {
+		startX = squareWidth;
+		startY = squareWidth;
+		endPointC[0] = endPointC[0] + (2 * squareWidth);
+		drawConnector(startX, startY, endPointC[0], endPointC[1], bezierX, bezierY, randomZeroOrOne[2]);
+		console.log("drawBlock complete");
+		cornerCounter++;
+	}
+ }
+
+function drawBlock2() {
+ 	if (cornerCounter == 10) {
+		drawBorder(squareWidth, squareWidth);
+		bezierY = (squareWidth / 2) * 3;		
+		cornerCounter++;
+	}
+	else if (cornerCounter == 11) {
+		startX = squareWidth;
+		startY = squareWidth;
+		endPointC[1] = (2 * squareWidth) - endPointC[1];
+		drawConnector(startX, startY, endPointC[0], endPointC[1], bezierX, bezierY, randomZeroOrOne[2]);
+		cornerCounter++;
+	} else if (cornerCounter == 12) {
+		startX = 2 * squareWidth;
+		startY = squareWidth;
+		endPointD[1] = endPointD[1] + (2 * squareWidth);
+		drawConnector(startX, startY, endPointD[0], endPointD[1], bezierX, bezierY, randomZeroOrOne[3]);
+		cornerCounter++;
+	} else if (cornerCounter == 13) {
+		startX = 2 * squareWidth;
+		startY = 2 * squareWidth;
+		endPointA[1] = (2 * squareWidth) - endPointA[1];
+		drawConnector(startX, startY, endPointA[0], endPointA[1], bezierX, bezierY, randomZeroOrOne[0]);
+		cornerCounter++;
+	} else if (cornerCounter == 14) {
+		startX = squareWidth;
+		startY = 2 * squareWidth;
+		drawConnector(startX, startY, endPointB[0], endPointB[1], bezierX, bezierY, randomZeroOrOne[1]);
+		console.log("drawBlock complete");
+		cornerCounter++;
+	}
+ }
+
+function drawBlock3() {
+ 	if (cornerCounter == 15) {
+		drawBorder(0, squareWidth);
+		bezierX = squareWidth / 2;		
+		cornerCounter++;
+	}
+	else if (cornerCounter == 16) {
+		startX = 0;
+		startY = squareWidth;
+		endPointD[0] = (2 * squareWidth) - endPointD[0];
+		drawConnector(startX, startY, endPointD[0], endPointD[1], bezierX, bezierY, randomZeroOrOne[3]);
+		cornerCounter++;
+	} else if (cornerCounter == 17) {
+		startX = squareWidth;
+		startY = squareWidth;
+		endPointC[0] = 0;
+		drawConnector(startX, startY, endPointC[0], endPointC[1], bezierX, bezierY, randomZeroOrOne[2]);
+		cornerCounter++;
+	} else if (cornerCounter == 18) {
+		startX = squareWidth;
+		startY = 2 * squareWidth;
+		endPointB[0] = (2 * squareWidth) - endPointB[0];
+		drawConnector(startX, startY, endPointB[0], endPointB[1], bezierX, bezierY, randomZeroOrOne[1]);
+		cornerCounter++;
+	} else if (cornerCounter == 19) {
+		startX = 0;
+		startY = 2 * squareWidth;
+		drawConnector(startX, startY, endPointA[0], endPointA[1], bezierX, bezierY, randomZeroOrOne[0]);
+		console.log("drawBlock complete");
+		cycleCounter++;
+		cornerCounter = 0;
+	}
+}
+
+ function drawFourBlocks(translateX, translateY) {
+ 	translate(translateX, translateY);
+ 	drawBlock0();
+	drawBlock1();
+	drawBlock2();
+	drawBlock3();
+}
+
+function drawSixteenBlocks() {
+	if (cycleCounter == 0) {
+		drawFourBlocks(0, 0);
+	} else if (cycleCounter == 1) {
+		drawFourBlocks(2 * squareWidth, 0);
+	} else if (cycleCounter == 2) {
+		drawFourBlocks(2 * squareWidth, 2 * squareWidth);
+	} else if (cycleCounter == 3) {
+		drawFourBlocks(0, 2 * squareWidth);
+	}
+}
+
+function refreshSketch() {
+	frameRate(frameRateSlider.value());
+	background(255);
+	cornerCounter = 0;
+	cycleCounter = 0;
+	// Renerate randomness
+	for (i = 0; i < 5; i++) {
+		randomFloat[i] = random(0, .9 * squareWidth);
+		console.log(random[i]);
+	}
+	delay.delayTime(100);
+	drawSixteenBlocks();
+}
